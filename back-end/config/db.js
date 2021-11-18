265f76997abd151fd80cbc32339d23d1e48d8096
Hieu <hieu.nguyen190125@hcmut.edu.vn>
2021-11-18 23:36:08 +0700

    db

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {});
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
