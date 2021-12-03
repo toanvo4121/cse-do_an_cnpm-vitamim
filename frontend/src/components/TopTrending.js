8e881f2daec53b2bd0796c56d99c5ca264d33c9e
Halee1505 <linh.nguyen1505@hcmut.edu.vn>
2021-12-03 23:59:42 +0700

    lam top trending

--- frontend/src/components/TopTrending.js
+++ frontend/src/components/TopTrending.js
@@ -1,21 +1,60 @@
 import React from 'react';
-
+import { Carousel } from "react-bootstrap";
 import './style.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {TopTrend} from "../Constant/Variable"
+import { TopTrend } from "../Constant/Variable"
 
 function TopTrending() {
     return (
         <div className="top-trending">
             <p className="top-trending_name">Top trending</p>
-            <div className="top-trending_overlay">
-                <img src="source/back.png" alt="" className="move" />
-                {
-                    TopTrend.map((trend, index) =>
-                        <img className="trend" alt="" id={index} key={index} src={trend.src}></img>
-                    )}
-                <img src="source/next.png" alt="" className="move" />
-            </div>
+
+            <Carousel >
+                <Carousel.Item >
+                <div className="trending">
+                        {TopTrend.map((trend, index) =>
+                            <div className="trend" style={{ backgroundImage: ('url(' + String(trend.src) + ')') }}>
+                                <div className="hover">
+                                    <img className="download-icon" alt="" src="source/download.png" />
+                                    <p>avt</p>
+                                    <p>username</p>
+                                    <p>caption</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item >
+                    <div className="trending">
+                        {TopTrend.map((trend, index) =>
+                            <div className="trend" style={{ backgroundImage: ('url(' + String(trend.src) + ')') }}>
+                                <div className="hover">
+                                    <img className="download-icon" alt="" src="source/download.png" />
+                                    <p>avt</p>
+                                    <p>username</p>
+                                    <p>caption</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item >
+                <div className="trending">
+                        {TopTrend.map((trend, index) =>
+                            <div className="trend" style={{ backgroundImage: ('url(' + String(trend.src) + ')') }}>
+                                <div className="hover">
+                                    <img className="download-icon" alt="" src="source/download.png" />
+                                    <p>avt</p>
+                                    <p>username</p>
+                                    <p>caption</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Carousel.Item>
+            </Carousel>
+
         </div>
     )
 }
