138232bdaaa764c1e0478f0da418837aa6ee4533
CuongFlodric <84664656+CuongFlodric@users.noreply.github.com>
2021-12-04 13:39:53 +0700

    Update TopTrending.js

--- frontend/src/components/TopTrending.js
+++ frontend/src/components/TopTrending.js
@@ -28,12 +28,56 @@ function InterchangeSort(a, n, name, avt, id) {
     }
 }
 
+function timeCalculate(time) {
+    let d = new Date()
+    if (time !== null) {
+        let x;
+        let ngay = time.split("T")[0].split("-")
+        let gio = time.split("T")[1].split(".")[0].split(":")
+        if (parseInt(gio[0]) + 7 >= 24) {
+            gio[0] = parseInt(gio[0]) + 7 - 24
+            ngay[2] = parseInt(ngay[2]) + 1
+            if (ngay[2] === 31 && parseInt(ngay[1]) === 11) {
+                ngay[2] = 1
+                ngay[1] = parseInt(12)
+            }
+        }
+        else {
+            gio[0] = parseInt(gio[0]) + 7
+        }
+
+
+        (d.getHours() - parseInt(gio[0]) === 0) ?
+            (x = String(d.getMinutes() - parseInt(gio[1]) <= 0 ? "Vừa xong" :
+                (d.getMinutes() - parseInt(gio[1]) + " phút"))) :
+            ((d.getDate() - parseInt(ngay[2]) === 0) ?
+                (x = String(d.getHours() - parseInt(gio[0])) + " giờ") :
+                (x = String(ngay[2] + "/" + ngay[1] + "/" + ngay[0])))
+
+        return x
+    }
+    else return d;
+}
 
 function pushPosts(a, n, scr, id) {
     var j, count = 0
     for (j = n - 1; j >= 0; j--) {
         if (a[j].user === id) {
             scr.push(a[j].mim_src)
+            //time.push(a[j].createdAt)
+            count = count + 1
+            if (count === 3)
+                break
+        }
+    }
+}
+
+function pushTimes(a, n, scr, id) {
+    var j, count = 0
+    for (j = n - 1; j >= 0; j--) {
+        if (a[j].user === id) {
+            scr.push(a[j].createdAt)
+            //time.push(a[j].createdAt)
             count = count + 1
             if (count === 3)
                 break
@@ -57,7 +101,6 @@ function TopTrending() {
         })
     }
 
-
     let name = []
     let avt = []
     let id = []
@@ -72,15 +115,22 @@ function TopTrending() {
     let Top1 = []
     let Top2 = []
     let Top3 = []
+    let TimeTop1= []
+    let TimeTop2= []
+    let TimeTop3= []
 
     if (Posts) {
         pushPosts(Posts, Posts.length, Top1, id[0])
         pushPosts(Posts, Posts.length, Top2, id[1])
         pushPosts(Posts, Posts.length, Top3, id[2])
+        pushTimes(Posts, Posts.length, TimeTop1, id[0])
+        pushTimes(Posts, Posts.length, TimeTop2, id[1])
+        pushTimes(Posts, Posts.length, TimeTop3, id[2])
     }
+    
+    console.log("c"+TimeTop1)
 
-
-    if (Top1 && Top2 && Top3) {
+    if (Top1 && Top2 && Top3 && TimeTop1 && TimeTop2 && TimeTop3) {
         
         return (
             <div className="top-trending">
@@ -94,6 +144,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[0]} alt="" />
                                             <p className="trending-name">{name[0]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* { <p className="thoigian">
+                                            {timeCalculate(TimeTop1[0])}
+                                        </p> } */}
                                     </div>
                                </div>
 
@@ -103,6 +157,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[0]} alt="" />
                                             <p className="trending-name">{name[0]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop1[1])}
+                                        </p> */}
                                     </div>
                                </div>
  
@@ -112,6 +170,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[0]} alt="" />
                                             <p className="trending-name">{name[0]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop1[2])}
+                                        </p> */}
                                     </div>
                                </div>
                         </div>
@@ -124,6 +186,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[1]} alt="" />
                                             <p className="trending-name">{name[1]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop2[0])}
+                                        </p> */}
                                     </div>
                                </div>
                         
@@ -134,6 +200,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[1]} alt="" />
                                             <p className="trending-name">{name[1]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop2[1])}
+                                        </p> */}
                                     </div>
                                </div>
 
@@ -144,6 +214,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[1]} alt="" />
                                             <p className="trending-name">{name[1]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop2[2])}
+                                        </p> */}
                                     </div>
                                </div>
                         </div>
@@ -156,6 +230,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[2]} alt="" />
                                             <p className="trending-name">{name[2]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop3[0])}
+                                        </p> */}
                                     </div>
                                </div>
 
@@ -165,6 +243,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[2]} alt="" />
                                             <p className="trending-name">{name[2]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop3[1])}
+                                        </p> */}
                                     </div>
                                </div>
 
@@ -174,6 +256,10 @@ function TopTrending() {
                                             <img className="trending-avt" src={avt[2]} alt="" />
                                            <p className="trending-name">{name[2]}</p>
                                         </div>
+                                        <img className="timer" src="https://res.cloudinary.com/vitamim/image/upload/v1637943120/source/clock_fqwtxq.png" alt="" />
+                                        {/* <p className="thoigian">
+                                            {timeCalculate(TimeTop3[2])}
+                                        </p> */}
                                     </div>
                                </div>
                         </div>
