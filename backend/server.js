e1b5a7f38fc5b7a6fb2807c88fa8ad88d4355421
Võ Minh Toàn <toanvo7120@gmail.com>
2021-12-06 23:09:13 +0700

    Version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,4 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-
-app.listen(PORT, function () {
-  console.log('Server is running on Port:', PORT);
-});
+app.listen(process.env.PORT || 5000)
