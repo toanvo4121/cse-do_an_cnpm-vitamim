198e62090dcd9d4c042e6fe7c878ef3c50bbc4f6
Võ Minh Toàn <toanvo7120@gmail.com>
2021-12-06 23:21:49 +0700

    Version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -41,10 +41,10 @@ app.use('/upload', UploadImg);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
